ecf96a850e6e3e771f756cd6711b9c04f296c4f6
AntonZenin <zenin.anton.2005@gmail.com>
2025-11-18 00:08:13 +0300

    fixed cognitive complexity, mb it will help

--- tasks/zenin_a_sum_values_by_columns_matrix/mpi/src/ops_mpi.cpp
+++ tasks/zenin_a_sum_values_by_columns_matrix/mpi/src/ops_mpi.cpp
@@ -31,6 +31,42 @@ bool ZeninASumValuesByColumnsMatrixMPI::PreProcessingImpl() {
   return true;
 }
 
+std::tuple<size_t, size_t> ZeninASumValuesByColumnsMatrixMPI::CalculateProcessColumns(int rank, int world_size, size_t columns) {
+  size_t base_cols_per_process = columns / world_size;
+  size_t remain = columns % world_size;
+
+  size_t start_column = rank * base_cols_per_process;
+  size_t cols_this_process = base_cols_per_process;
+
+  if (rank == world_size - 1) {
+    cols_this_process += remain;
+  }
+  return {start_column, cols_this_process};
+}
+
+void ZeninASumValuesByColumnsMatrixMPI::CalculateLocalSums(const std::vector<double> &matrix_data, 
+  size_t columns, size_t total_rows, size_t start_column, size_t cols_this_process, std::vector<double> &local_sums) {
+  for (size_t local_column = 0; local_column < cols_this_process; ++local_column) {
+    size_t global_col = start_column + local_column;
+    for (size_t row = 0; row < total_rows; ++row) {
+      local_sums[local_column] += matrix_data[(row * columns) + global_col];
+    }
+  }
+}
+
+void ZeninASumValuesByColumnsMatrixMPI::PrepareGathervParameters(int world_size, size_t base_cols_per_process, 
+  size_t remain, std::vector<int> &recv_counts, std::vector<int> &displacements) {
+  for (int i = 0; i < world_size; ++i) {
+    recv_counts[i] = static_cast<int>(base_cols_per_process);
+    if (i == world_size - 1) {
+      recv_counts[i] += static_cast<int>(remain);
+    }
+    if (i > 0) {
+      displacements[i] = displacements[i - 1] + recv_counts[i - 1];
+    }
+  }
+}
+
 bool ZeninASumValuesByColumnsMatrixMPI::RunImpl() {
   auto &input = GetInput();
   int world_size = 0;
@@ -54,22 +90,9 @@ bool ZeninASumValuesByColumnsMatrixMPI::RunImpl() {
 
   MPI_Bcast(&columns, 1, MPI_UNSIGNED_LONG, 0, MPI_COMM_WORLD);
   MPI_Bcast(&total_rows, 1, MPI_UNSIGNED_LONG, 0, MPI_COMM_WORLD);
+
   if (columns == 0) {
     return false;
-  }
-
-  size_t base_cols_per_process = columns / world_size;
-  size_t remain = columns % world_size;
-
-  size_t start_column = 0;
-  size_t cols_this_process = 0;
-
-  if (rank == world_size - 1) {
-    start_column = rank * base_cols_per_process;
-    cols_this_process = base_cols_per_process + remain;
-  } else {
-    start_column = rank * base_cols_per_process;
-    cols_this_process = base_cols_per_process;
   }
 
   if (rank != 0) {
@@ -77,40 +100,29 @@ bool ZeninASumValuesByColumnsMatrixMPI::RunImpl() {
   }
   MPI_Bcast(matrix_data.data(), static_cast<int>(matrix_data.size()), MPI_DOUBLE, 0, MPI_COMM_WORLD);
 
+  auto [start_column, cols_this_process] = CalculateProcessColumns(rank, world_size, columns);
   std::vector<double> local_sums(cols_this_process, 0.0);
-
-  for (size_t local_column = 0; local_column < cols_this_process; ++local_column) {
-    size_t global_col = start_column + local_column;
-    for (size_t row = 0; row < total_rows; ++row) {
-      local_sums[local_column] += matrix_data[(row * columns) + global_col];
-    }
-  }
+  CalculateLocalSums(matrix_data, columns, total_rows, start_column, cols_this_process, local_sums);
 
   std::vector<double> global_sums;
   if (rank == 0) {
     global_sums.resize(columns, 0.0);
   }
 
+  size_t base_cols_per_process = columns / world_size;
+  size_t remain = columns % world_size;
+
   std::vector<int> recv_counts(world_size, 0);
   std::vector<int> displacements(world_size, 0);
 
   if (rank == 0) {
-    for (int i = 0; i < world_size; ++i) {
-      if (i == world_size - 1) {
-        recv_counts[i] = static_cast<int>(base_cols_per_process + remain);
-      } else {
-        recv_counts[i] = static_cast<int>(base_cols_per_process);
-      }
-
-      if (i > 0) {
-        displacements[i] = displacements[i - 1] + recv_counts[i - 1];
-      }
-    }
+    PrepareGathervParameters(world_size, base_cols_per_process, remain, recv_counts, displacements);
   }
 
   MPI_Gatherv(local_sums.data(), static_cast<int>(local_sums.size()), MPI_DOUBLE, global_sums.data(),
               recv_counts.data(), displacements.data(), MPI_DOUBLE, 0, MPI_COMM_WORLD);
 
+  
   if (rank != 0) {
     global_sums.resize(columns);
   }
