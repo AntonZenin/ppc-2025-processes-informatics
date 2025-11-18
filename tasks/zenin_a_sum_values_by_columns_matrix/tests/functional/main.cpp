2102816163c926cbb98f865368b849b493c36ee7
AntonZenin <zenin.anton.2005@gmail.com>
2025-11-18 11:33:15 +0300

    added more tests

--- tasks/zenin_a_sum_values_by_columns_matrix/tests/functional/main.cpp
+++ tasks/zenin_a_sum_values_by_columns_matrix/tests/functional/main.cpp
@@ -93,8 +93,8 @@ TEST_P(ZeninASumValuesByMatrixFunctTests, SumByColumnsTest) {
   ExecuteTest(GetParam());
 }
 
-const std::array<TestType, 5> kTestParam = {std::string("matrix1"), std::string("matrix2"), std::string("matrix3"),
-                                            std::string("matrix4"), std::string("matrix5")};
+const std::array<TestType, 6> kTestParam = {std::string("matrix1"), std::string("matrix2"), std::string("matrix3"),
+                                            std::string("matrix4"), std::string("matrix5"), std::string("matrix6")};
 
 const auto kTestTasksList = std::tuple_cat(ppc::util::AddFuncTask<ZeninASumValuesByColumnsMatrixMPI, InType>(
                                                kTestParam, PPC_SETTINGS_zenin_a_sum_values_by_columns_matrix),
